76cdedf8862ee05323005bd7a937a090e7089818
Carlos Strand <carlosstrand@gmail.com>
2017-12-03 21:03:59 -0300

    Adding main parser

--- src/index.js
+++ src/index.js
@@ -4,6 +4,60 @@ import lexer from 'node-c-lexer';
 
 const isIfExpressionValid = (ifExpressionToken) => true;
 
+
+const findNextCloseBracketIndex = (token) => {
+  let openBracketCount = 0;
+  let closeBracketCount = 0;
+  let closeBracketIndex = null;
+  token.forEach((t, idx) => {
+    if (t.lexeme === '{') {
+      openBracketCount++;
+    } else if (t.lexeme === '}') {
+      closeBracketCount++;
+    }
+    if (closeBracketIndex === null && openBracketCount === closeBracketCount && openBracketCount !== 0) {
+      closeBracketIndex = idx;
+    }
+  });
+  if (openBracketCount !== closeBracketCount) {
+    return null;
+    console.log('Erro! Chaves não estão balanceadas!');
+  }
+  if (closeBracketIndex === null && openBracketCount == 0) {
+    console.log('Não há abertura de chaves para o if');
+  }
+  return closeBracketIndex;
+}
+
+const mainParser = (token) => {
+
+  const blocks = {
+    if: [],
+  };
+
+  while (token.length !== 0) {
+    if (token[0].lexeme === 'if') {
+      const endIfIndex = findNextCloseBracketIndex(token);
+      if (!endIfIndex) {
+        return;
+      }
+      const ifBlock = token.slice(0, endIfIndex + 1);
+      blocks.if.push(ifBlock);
+      token = token.slice(endIfIndex + 1);
+    }
+  }
+
+  let parserStatus = true;
+  blocks.if.forEach(ifBlock => {
+    parserStatus = parserStatus && parseIf(ifBlock);
+  });
+
+  return parserStatus;
+
+}
+
+
+
 const parseIf = (token) => {
   if (token.length === 0) {
     return true;
@@ -76,7 +130,7 @@ const parseIf = (token) => {
     console.log('Erro: a condição booleana do if não é válida');
     return;
   }
-  return parseIf(ifBlockToken);
+  return !!mainParser(ifBlockToken);
 }
 
 
@@ -85,8 +139,17 @@ const code = `
     if (b==10) {
     }
   }
+  if (Z == 9) {
+    if (y == 20) {
+      if (y == 30) {
+
+      }
+    }
+  }
 `;
 
 const token = lexer.lexUnit.tokenize(code);
 
-console.log(parseIf(token));
+// console.log(parseIf(token));
+console.log(mainParser(token));
+// console.log(findCloseBracketIndex(token));
